feat(utils): fix eta calculation

src/od.py
@@ -12,16 +12,16 @@ import copy
 
 # TODO: Maybe merge all scikit transformers and use scikit pipeline?
 # TODO: Check https://stackoverflow.com/a/48807797/6253183 to streamline OD and CLF steps
-def apply_pca(initial_matrices: Dataset, **kwargs) -> Dataset:
-    debug(f'Applying PCA on {train_size(initial_matrices)}')
+def apply_pca(dfs: Dataset, **kwargs) -> Dataset:
+    debug(f'Applying PCA on {train_size(dfs):e}')
     # pca = PCA(n_components=CONFIG['pca']['n_components'])   # TODO: Non spare `scipy.sparse.issparse(my_matrix)`
     transformer = TruncatedSVD(**kwargs)  # for sparse only
-    transformer.fit(initial_matrices['train'])
-    return {dataset: transformer.transform(df) for dataset, df in initial_matrices.items()}
+    transformer.fit(dfs['train'])
+    return {dataset: transformer.transform(df) for dataset, df in dfs.items()}
 
 
 def apply_scaler(dfs: Dataset, **kwargs) -> Dataset:
-    debug(f'Applying Scaler on {train_size(dfs)}')
+    debug(f'Applying Scaler on {train_size(dfs):e}')
     transformer = StandardScaler(with_mean=False, **kwargs)
     # ValueError: Cannot center sparse matrices: pass `with_mean=False` instead. See docstring for motivation and alternatives.
     # TODO: perform differently for sparse
@@ -29,15 +29,14 @@ def apply_scaler(dfs: Dataset, **kwargs) -> Dataset:
     return {dataset: transformer.transform(df) for dataset, df in dfs.items()}
 
 
-def apply_none(initial_matrices: Dataset, **kwargs) -> Dataset:
-    return copy.deepcopy(initial_matrices)
+def apply_none(dfs: Dataset, **kwargs) -> Dataset:
+    return copy.deepcopy(dfs)
 
 
-def apply_od(initial_matrices: Dataset, method: str, **kwargs) -> Dataset:
-    debug(f'Applying OD on {train_size(initial_matrices)}')
+def apply_od(dfs: Dataset, method: str, **kwargs) -> Dataset:
     return {
         'none': apply_none,
         'pca': apply_pca,
         'scaled': apply_scaler,  # TODO: Probably change scaled as the "none" default
         # and come up with some other OD
-    }[method](initial_matrices, **kwargs)
+    }[method](dfs, **kwargs)